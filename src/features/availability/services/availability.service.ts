517d6c2a9d12983b17a593b149ec1a33da1500c5
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2023-05-12 11:25:24 +0200

    feat: add service functionality

--- src/features/availability/services/availability.service.ts
+++ src/features/availability/services/availability.service.ts
@@ -3,14 +3,14 @@ import api from '@/common/utils/axiosInstance';
 import AvailabilitySearchDto from "../types/searchDto";
 
 export const createAvailability = async(dto: AvailabilityDto) =>{
-
+    return await api.post('/api/avail/create', dto);
 }
 
 export const updateAvailability = async(dto: AvailabilityDto)=>{
-
+    return await api.post('/api/avail/update', dto);
 }
 export const deleteAvailability = async(id: string)=>{
-    
+    return await api.post('/api/avail/delete/'+ id);
 }
 
 export const GetAllSearch = async (dto: AvailabilitySearchDto)=>{
@@ -20,5 +20,5 @@ export const GetAllForUser = async (email: string)=>{
     
 }
 export const GetById = async (id: string)=>{
-    
+    return await api.post('/api/avail/id/'+ id);
 }
