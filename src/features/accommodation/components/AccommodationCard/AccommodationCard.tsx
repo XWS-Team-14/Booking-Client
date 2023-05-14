0f6d58f6ea685fe3bb90bd6e9e4202cccb8e1b6b
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-05-14 18:10:59 +0200

    feat: add reserve button to accommodation component

--- src/features/accommodation/components/AccommodationCard/AccommodationCard.tsx
+++ src/features/accommodation/components/AccommodationCard/AccommodationCard.tsx
@@ -3,13 +3,17 @@ import { Avatar, Card, Divider } from 'antd';
 const { Meta } = Card;
 //Missing props.
 
+import Button from '@/common/components/button/Button';
+import { selectRole } from '@/common/store/slices/authSlice';
 import UserIcon from '@/features/user/components/icon/UserIcon';
+import { useSelector } from 'react-redux';
 import { AccommodationAmenity } from './AccommodationAmenity';
 import styles from './AccommodationCard.module.scss';
 import { AccommodationHeader } from './AccommodationHeader';
 import { AccommodationImages } from './AccommodationImages';
 
 const AccommodationCard = () => {
+  const role = useSelector(selectRole);
   const imagesExample = [
     'https://cf.bstatic.com/xdata/images/hotel/square600/255152681.webp?k=b1f184a5e6de8a12196b0f0a18b76a6a93db7c65cce2d0dd4507ab2a606ef539&o=&s=1',
     'https://cf.bstatic.com/xdata/images/hotel/max1280x900/399559679.jpg?k=4d88298c3e2495748558ebd7039459f2c77e9a3709a81266c2784ab61eca4cb1&o=&hp=1',
@@ -39,6 +43,15 @@ const AccommodationCard = () => {
         Host
       </Divider>
       <Avatar src={<UserIcon type="male" size={0} />} /> Mr. Host
+      {role === 'guest' && (
+        <>
+          <Button
+            type="primary"
+            text="Reserve"
+            style={{ width: '100%', marginTop: '2rem' }}
+          />
+        </>
+      )}
     </Card>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import BaseTemplate from '@/common/components/baseTemplate/BaseTemplate';
+import AccommodationCard from '@/features/accommodation/components/AccommodationCard/AccommodationCard';
 import Search from '@/features/search/components/Search';
 
 export default function HomePage() {
   return (
     <BaseTemplate>
       <Search />
+      <AccommodationCard />
     </BaseTemplate>
   );
 }
