1e15bf07f198ef942ac6be430cfe5641dd9ef64b
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-06-14 14:35:08 +0200

    feat: design

--- src/features/search/components/SearchBar.tsx
+++ src/features/search/components/SearchBar.tsx
@@ -2,7 +2,9 @@ import Button from '@/common/components/button/Button';
 import Loading from '@/common/components/loading/Loading';
 import { disabledDateRangePicker } from '@/common/utils/dateHelper';
 import { SearchOutlined } from '@ant-design/icons';
-import { DatePicker, Input, InputNumber } from 'antd';
+import { Checkbox, Collapse, DatePicker, Input, InputNumber } from 'antd';
+
+import CollapsePanel from 'antd/lib/collapse/CollapsePanel';
 import { RangePickerProps } from 'antd/lib/date-picker';
 import { useEffect, useState } from 'react';
 import styles from '../styles/search.module.scss';
@@ -18,6 +20,7 @@ const SearchBar = ({ onDataChanged }: SearchBarProps) => {
   useEffect(() => {
     setLoading(false);
   }, []);
+
   function sendDataToParent() {
     onDataChanged(searchParams);
   }
@@ -82,101 +85,135 @@ const SearchBar = ({ onDataChanged }: SearchBarProps) => {
   return loading ? (
     <Loading />
   ) : (
-    <div className={styles.searchBarContainer}>
-      <Input
-        allowClear
-        placeholder="Country"
-        bordered={false}
-        style={{
-          width: '70%',
-          backgroundColor: 'white',
-          padding: '1.2rem',
-          borderRadius: '0',
-          borderBottomLeftRadius: '2rem',
-          borderTopLeftRadius: '2rem',
-          height: '54.4px',
-        }}
-        onChange={(e) => {
-          changeCountry(e.target.value);
-        }}
-      />
-      <Input
-        placeholder="City"
-        bordered={false}
-        allowClear
-        style={{
-          width: '70%',
-          backgroundColor: 'white',
-          padding: '0.7rem',
-          borderRadius: '0',
-          height: '54.4px',
-        }}
-        onChange={(e) => {
-          changeCity(e.target.value);
-        }}
-      />
-      <Input
-        placeholder="Address"
-        bordered={false}
-        allowClear
-        style={{
-          width: '70%',
-          backgroundColor: 'white',
-          padding: '0.7rem',
-          borderRadius: '0',
-          height: '54.4px',
-        }}
-        onChange={(e) => {
-          changeAddress(e.target.value);
-        }}
-      />
-      <DatePicker.RangePicker
-        format="dddd, MMMM DD, YYYY"
-        allowClear
-        placeholder={['Check-in date', 'Checkout date']}
-        disabledDate={disabledDateRangePicker}
-        style={{
-          width: '130%',
-          backgroundColor: 'white',
-          padding: '0.7rem',
-          borderWidth: '0',
-          borderRadius: '0',
-          height: '54.4px',
-        }}
-        onChange={(e) => {
-          changeDate(e, ['YYYY-MM-DD', 'YYYY-MM-DD']);
-        }}
-      />
+    <>
+      <div className={styles.searchBarContainer}>
+        <Input
+          allowClear
+          placeholder="Country"
+          bordered={false}
+          style={{
+            width: '70%',
+            backgroundColor: 'white',
+            padding: '1.2rem',
+            borderRadius: '0',
+            borderBottomLeftRadius: '2rem',
+            borderTopLeftRadius: '2rem',
+            height: '54.4px',
+          }}
+          onChange={(e) => {
+            changeCountry(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="City"
+          bordered={false}
+          allowClear
+          style={{
+            width: '70%',
+            backgroundColor: 'white',
+            padding: '0.7rem',
+            borderRadius: '0',
+            height: '54.4px',
+          }}
+          onChange={(e) => {
+            changeCity(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="Address"
+          bordered={false}
+          allowClear
+          style={{
+            width: '70%',
+            backgroundColor: 'white',
+            padding: '0.7rem',
+            borderRadius: '0',
+            height: '54.4px',
+          }}
+          onChange={(e) => {
+            changeAddress(e.target.value);
+          }}
+        />
+        <DatePicker.RangePicker
+          format="dddd, MMMM DD, YYYY"
+          allowClear
+          placeholder={['Check-in date', 'Checkout date']}
+          disabledDate={disabledDateRangePicker}
+          style={{
+            width: '130%',
+            backgroundColor: 'white',
+            padding: '0.7rem',
+            borderWidth: '0',
+            borderRadius: '0',
+            height: '54.4px',
+          }}
+          onChange={(e) => {
+            changeDate(e, ['YYYY-MM-DD', 'YYYY-MM-DD']);
+          }}
+        />
 
-      <InputNumber
-        min={0}
-        max={15}
-        placeholder="Number of guests"
+        <InputNumber
+          min={0}
+          max={15}
+          placeholder="Number of guests"
+          bordered={false}
+          style={{
+            width: '70%',
+            backgroundColor: 'white',
+            padding: '0.7rem',
+            borderRadius: '0',
+            height: '54.4px',
+          }}
+          onChange={(e) => {
+            changeGuests(e);
+          }}
+        />
+        <Button
+          action={sendDataToParent}
+          type="primary"
+          style={{
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+            height: '54.4px',
+            paddingRight: '2rem',
+          }}
+        >
+          <SearchOutlined style={{ fontSize: '24px' }} />
+        </Button>
+      </div>
+      <Collapse
+        defaultActiveKey={['1']}
+        ghost
         bordered={false}
-        style={{
-          width: '70%',
-          backgroundColor: 'white',
-          padding: '0.7rem',
-          borderRadius: '0',
-          height: '54.4px',
-        }}
-        onChange={(e) => {
-          changeGuests(e);
-        }}
-      />
-      <Button
-        action={sendDataToParent}
-        type="primary"
-        style={{
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: 0,
-          height: '54.4px',
-          paddingRight: '2rem',
-        }}
+        style={{ width: '100%', maxWidth: '1240px' }}
       >
-        <SearchOutlined style={{ fontSize: '24px' }} />
-      </Button>
-    </div>
+        <CollapsePanel key={'1'} header="Filters" style={{ width: '100%' }}>
+          <div className={styles.filters}>
+            <div className={styles.filters__priceRange}>
+              <Checkbox>
+                <b>Price range</b>
+              </Checkbox>
+              <div className={styles.filters__priceRange__inputs}>
+                <InputNumber prefix="€" placeholder="Min"></InputNumber>–
+                <InputNumber prefix="€" placeholder="Max"></InputNumber>
+              </div>
+            </div>
+            <div className={styles.filters__amenities}>
+              <b>Amenities</b>
+              <div className={styles.filters__amenities__items}>
+                <Checkbox>Wi-Fi</Checkbox>
+                <Checkbox>Flat-screen TV</Checkbox>
+              </div>
+            </div>
+            <div>
+              <Checkbox>
+                <b>Featured host</b>
+              </Checkbox>
+            </div>
+          </div>
+        </CollapsePanel>
+      </Collapse>
+    </>
   );
 };
 
